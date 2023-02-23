806cd9a168f99c66cabcd9b65d122ecc000e4df5
gsmolinski <g.smolinski1@gmail.com>
2023-02-23 11:44:35 +0100

    set up connection to postgresql database

--- R/app_server.R
+++ R/app_server.R
@@ -5,6 +5,20 @@
 #' @import shiny
 #' @noRd
 app_server <- function(input, output, session) {
+
+  config_db <- config::get("glif_db", file = system.file(package = "glif", "database-config.yml"))
+
+  glif_db <- DBI::dbConnect(RPostgres::Postgres(),
+                            dbname = config_db$dbname,
+                            host = config_db$host,
+                            port = config_db$port,
+                            user = config_db$user,
+                            password = config_db$password)
+
+  onStop(function() {
+    DBI::dbDisconnect(glif_db)
+  })
+
   observe({
     session$sendCustomMessage("change_nav_text", input$glif_tabs)
   })
